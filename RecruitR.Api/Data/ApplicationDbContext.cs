c0304b002bcb7ec8941f066232851697809874bb
Bilal Saher <bilalsaher80@gmail.com>
2025-07-08 13:23:28 +0200

    Initial commit with ASP.NET Core API and Azure SQL connection

---
+++ RecruitR.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using RecruitR.Api.Models;
+
+namespace RecruitR.Api.Data
+{
+    public class  ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+        }
+
+        public DbSet<JobApplication> JopApplications { get; set; }
+
+    }
+}
